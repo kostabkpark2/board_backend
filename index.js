4ad95199ab8d939172172de61e6342014cd2c394
kostabkpark2 <kostabkpark2@gmail.com>
2025-11-11 09:28:53 +0900

    get board list

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = 8000;
 app.use(express.json());
 app.use(cors());
 
@@ -35,5 +35,5 @@ app.get("/boards", (req,res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log("localhost:3000 에서 서버가 실행중...");
+    console.log("localhost:8000 에서 서버가 실행중...");
 });
